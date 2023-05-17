de540909550a5d48eb02c8cbbb1e30d8546af492
Walid Oumechtak <woumecht@e1r3p3.1337.ma>
2023-05-17 19:39:33 +0100

    done with this mini crud app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          \App\Models\User::factory(10)->create();
+         \App\Models\Product::factory(20)->create();
         //$this->call(SeederProducts::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
